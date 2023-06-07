ac19c4245222f79938d5474dcfede4bd598e1bbd
Siddarth Pai <siddarthdpai@gmail.com>
2023-06-07 23:58:14 +0530

    created task-item-component

--- animated-todo/src/components/animated-checkbox.tsx
+++ animated-todo/src/components/animated-checkbox.tsx
@@ -1,6 +1,7 @@
 import React,{useEffect, memo} from 'react'
 import Animated,{Easing, useSharedValue, useAnimatedProps, withTiming, interpolateColor} from 'react-native-reanimated'
 import { Svg, Path, Defs, ClipPath, G} from 'react-native-svg'
+import AnimatedStroke from './animated-stroke'
 
 
 //define margins and the svg path for the checkmark box
@@ -11,20 +12,21 @@ const checkMarkPath = 'M8 32.5C18 39 26 47 26 47C26 47 33 28 63.5 4'
 const outlineBoxPath = 'M24 0.5H40C48.5809 0.5 54.4147 2.18067 58.1175.88299C61.8193 9.58532 63.5 15.4191 63.5 24V40C63.5 48.5809 61.8193 54.4147 58.117 58.117C54.4147 61.8193 48.5809 63.5 40 63.5H24C15.4191 63.5 9.58532 61.8193 5.88299 58.117C2.18067 54.4147 0.5 48.5809 0.5 40V24C0.5 15.4191 2.18067 9.58532 5.88299 5.88299C9.58532 2.18067 15.4191 0.5 24 0.5Z'
 const AnimatedPath = Animated.createAnimatedComponent(Path)
 interface Props{
-    checked?: boolean
+    checked?: boolean,
+    highlightColor: string,
+    checkmarkColor: string,
+    boxOutlineColor: string
 }
 
 const AnimatedCheckbox = (props: Props) => {
-    const {checked} = props
-    const checkmarkColor= "#000000"
-    const highlightColor = "#ff0000"
-    const boxOutlineColor = "#000000"
+    const {checked, checkmarkColor, highlightColor, boxOutlineColor} = props
 
     const progress = useSharedValue(0)
 
     useEffect(()=>{
         progress.value = withTiming(checked ? 1: 0, {
-            duration: Easing.linear
+            duration: checked? 300:100,
+            easing: Easing.linear
         })
     },[checked])
 
@@ -47,9 +49,50 @@ const AnimatedCheckbox = (props: Props) => {
 
     return(
         <Svg viewBox={[-MARGIN, -MARGIN, vWidth+ MARGIN, vHeight + MARGIN].join(' ')} fill='none'>
-            <Path d={outlineBoxPath} stroke="black"/>
-            <AnimatedPath d={outlineBoxPath} strokeWidth={7} strokeLinejoin="round" strokeLinecap="round" animatedProps={animatedBoxProps}/>
-            <Path d={checkMarkPath} stroke="black"/>
+            
+            <Defs>
+                <ClipPath id="clipPath">
+                    <Path
+                        fill="white" 
+                        stroke="gray"
+                        strokeLinejoin='round'
+                        strokeLinecap='round'
+                        d={outlineBoxPath}
+                    />
+                </ClipPath>
+            </Defs>
+
+            <AnimatedStroke 
+                progress={progress} 
+                d={checkMarkPath} 
+                stroke={highlightColor} 
+                strokeWidth={10} 
+                strokeLinejoin="round" 
+                strokeLinecap="round" 
+                strokeOpacity={checked || false? 1:0 }
+            />
+
+            <AnimatedPath 
+                d={outlineBoxPath} 
+                strokeWidth={7} 
+                strokeLinejoin="round" 
+                strokeLinecap="round" 
+                animatedProps={animatedBoxProps}
+            />
+
+
+            {/*to make the checkmar fit the box. */}
+            <G clipPath='url(#clipPath)'>
+            <AnimatedStroke 
+                progress={progress} 
+                d={checkMarkPath} 
+                stroke={checkmarkColor} 
+                strokeWidth={10} 
+                strokeLinejoin="round" 
+                strokeLinecap="round" 
+                strokeOpacity={checked || false? 1:0 }
+            />
+            </G>
         </Svg>
     )
 }
--- animated-todo/src/screens/main.tsx
+++ animated-todo/src/screens/main.tsx
@@ -1,4 +1,5 @@
-import * as React from 'react';
+import * as React from 'react'
+import { Pressable } from 'react-native';
 import{
     Text,
     Box,
@@ -8,15 +9,22 @@ import{
 }from 'native-base'
 import ThemeToggle from '../components/theme-toggle';
 import AnimatedCheckbox from '../components/animated-checkbox';
+import TaskItem from '../components/task-item'
 
 export default function MainScreen(){
+    //to enable the checkbox to be ticked.
+    const [checked, setChecked] = React.useState(false)
+    const handlePressCheckbox = React.useCallback(()=>{
+        setChecked(prev => !prev)
+    })
+
     return (
         //here we define the color for the dark and light mode
         //we set background color to red if light mode and yellow for dark mode
         <Center _dark={{bg:'blueGray.900'}} _light={{bg:'blueGray.50'}} px={4} flex={1}>
             <VStack space={5} alignItems="center">
-                <Box w="100px" h="100px">
-                    <AnimatedCheckbox/>
+                <Box w="100px" h="100px">  
+                    <TaskItem isDone={checked} onToggleCheckbox={handlePressCheckbox}/>  
                 </Box>
                 <Box p={10} bg={useColorModeValue('red.500','yellow.500')}>
                     <Text>
